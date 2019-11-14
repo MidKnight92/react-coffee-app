11414eb9a5c82393a0cb41087a5f2558ad92d37c
Stephanie Viveros <sviveros05@gmail.com>
2019-11-13 23:47:53 -0600

    Styled

--- src/CoffeeList/index.js
+++ src/CoffeeList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Segment } from 'semantic-ui-react';
+import { List, Segment } from 'semantic-ui-react'
 
 function CoffeeList(props) {
     const coffee = props.coffees.map((coffee) => {
@@ -8,9 +8,9 @@ function CoffeeList(props) {
 	            <List divided inverted relaxed>
 		        	<List.Item>
 		        		<List.Content>
-			        	<List.Header>{coffee.name}</List.Header>
-			        	From: {coffee.origin}
-			        	Acidity: {coffee.acidity}
+			        	<List.Header>{coffee.name}</List.Header> 
+			        	<List.Item className='origin'>From: {coffee.origin} </List.Item> 
+			        	<List.Item className='acidity'>Acidity: {coffee.acidity} </List.Item>
 			        </List.Content>	
 		        	</List.Item>
 	        	</List>
